808f75e825d72017ef150d4b650155edb2085d83
Jens Klingenberg <beispiel@posteo.de>
2018-06-15 20:40:41 +0200

    Initial commit

---
+++ app/src/main/java/de/jensklingenberg/jkmanagespaceactivity/ManageActivity.kt
@@ -0,0 +1,12 @@
+package de.jensklingenberg.jkmanagespaceactivity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ManageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
