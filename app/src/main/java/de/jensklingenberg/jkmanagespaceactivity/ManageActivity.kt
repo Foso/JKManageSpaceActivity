e77407fcaf75f1030dee62cb52c4ad42515e283c
Jens Klingenberg <beispiel@posteo.de>
2018-06-15 22:20:48 +0200

    change ManageActivity

--- app/src/main/java/de/jensklingenberg/jkmanagespaceactivity/ManageActivity.kt
+++ app/src/main/java/de/jensklingenberg/jkmanagespaceactivity/ManageActivity.kt
@@ -7,6 +7,6 @@ class ManageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_manage)
     }
 }
